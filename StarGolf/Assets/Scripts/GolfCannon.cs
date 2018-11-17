df301e3ee92105d92cf8998d43fc5c8a13fbb1be
Asger Stig Abildgaard <asgera02@gmail.com>
2018-11-17 20:20:04 +0100

    Smartere bolde
    De forvandler sig nu til [indsæt prefab her], når de har rullet i et kort stykke tid.

--- StarGolf/Assets/Scripts/GolfCannon.cs
+++ StarGolf/Assets/Scripts/GolfCannon.cs
@@ -10,9 +10,9 @@ public class GolfCannon : MonoBehaviour {
     static int currentPlayerID;
     int myID;
 	bool canshoot = true;
-	float CountClones = 0;
-    static int lastDoneFrame;
-	Renderer[] renderers = (Renderer[])(FindObjectsOfType(typeof(Renderer)));
+	int CountClones = 0;
+    public static float shootCooldown = 0;
+    public static int turnCount = 0;
 
 	public BallType[] ammonitionType;
 
@@ -26,40 +26,46 @@ public class GolfCannon : MonoBehaviour {
 	void Update()
 	{
 		
-		if (currentPlayerID == myID && lastDoneFrame != Time.frameCount)
+		if (currentPlayerID == myID)
 		{
-			Vector2 mousePos = cam.ScreenToWorldPoint(Input.mousePosition) - transform.position;
-			transform.rotation = Quaternion.Euler(0, 0, -Mathf.Atan2(mousePos.x, mousePos.y) * Mathf.Rad2Deg);
-
-			if (Input.GetKeyDown(KeyCode.Mouse0))
-			{
-				if (canshoot == true) ;
-				{
-					GameObject Ball = Instantiate(golfBallPrefab, transform.position, transform.rotation);
-					Ball.GetComponent<Rigidbody2D>().velocity = transform.rotation * Vector2.up * 5;
-					Ball.GetComponent<SpriteRenderer>().sprite = ammonitionType[0].sprite[myID];
-
-					EndTurn();
-				}
-
-				else
-				{
-					Debug.Log("You can not shoot right now!");
-				}
-			}
+            if (shootCooldown <= 0)
+            {
+		    	Vector2 mousePos = cam.ScreenToWorldPoint(Input.mousePosition) - transform.position;
+    			transform.rotation = Quaternion.Euler(0, 0, -Mathf.Atan2(mousePos.x, mousePos.y) * Mathf.Rad2Deg);
+    
+			    if (Input.GetKeyDown(KeyCode.Mouse0))
+			    {
+    				if (canshoot == true)
+				    {
+    					GameObject Ball = Instantiate(golfBallPrefab, transform.position, transform.rotation);
+					    Ball.GetComponent<Rigidbody2D>().velocity = transform.rotation * Vector2.up * 5;
+                        Ball.GetComponent<BallStuff>().Initialize(ammonitionType[0], 0, turnCount + 1);
+    
+                        shootCooldown = 15;
+					    EndTurn();
+				    }
+				    else
+				    {
+    					Debug.Log("You can not shoot right now!");
+				    }
+			    }
+            }
+            else
+            {
+                shootCooldown -= Time.deltaTime;
+            }
 		}
 
 	}
     void EndTurn()
     {
+        turnCount++;
         currentPlayerID++;
 
         if (currentPlayerID >= allPlayers.Count)
         {
             currentPlayerID = 0;
         }
-
-        lastDoneFrame = Time.frameCount;
 
         print(currentPlayerID);
     }
