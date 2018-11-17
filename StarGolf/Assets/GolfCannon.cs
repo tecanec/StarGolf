c5a48eb60ab13552d2d6880f0d6f2519ca0a6bd4
Pizzadrengen <39215307+Pizzadrengen@users.noreply.github.com>
2018-11-17 16:07:33 +0100

    1
    1

--- StarGolf/Assets/GolfCannon.cs
+++ StarGolf/Assets/GolfCannon.cs
@@ -3,13 +3,23 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class GolfCannon : MonoBehaviour {
+
+	//Cams
 	public Camera cam;
+
+	//Gameobjects
 	public GameObject golfBallPrefab;
 
+	//Lists
     static List<GolfCannon> allPlayers = new List<GolfCannon>();
-    static int currentPlayerID;
+
+	//Ints
+	static int currentPlayerID;
     int myID;
     static int lastDoneFrame;
+
+
+
 
 	// Use this for initialization
 	void Start () {
@@ -19,19 +29,21 @@ public class GolfCannon : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-        if (currentPlayerID == myID && lastDoneFrame != Time.frameCount)
-        {
-            Vector2 mousePos = cam.ScreenToWorldPoint(Input.mousePosition) - transform.position;
-            transform.rotation = Quaternion.Euler(0, 0, -Mathf.Atan2(mousePos.x, mousePos.y) * Mathf.Rad2Deg);
+		if (currentPlayerID == myID && lastDoneFrame != Time.frameCount)
+		{
+			Vector2 mousePos = cam.ScreenToWorldPoint(Input.mousePosition) - transform.position;
+			transform.rotation = Quaternion.Euler(0, 0, -Mathf.Atan2(mousePos.x, mousePos.y) * Mathf.Rad2Deg);
+		}
+            
     
-            if (Input.GetKeyDown(KeyCode.Mouse0))
-            {
-                GameObject Ball = Instantiate(golfBallPrefab, transform.position, transform.rotation);
-                Ball.GetComponent<Rigidbody2D>().velocity = transform.rotation * Vector2.up * 5;
-                    
-                EndTurn();
-            }
+        if (Input.GetKeyDown(KeyCode.Mouse0))
+        {
+			GameObject Ball = Instantiate(golfBallPrefab, transform.position, transform.rotation);
+            Ball.GetComponent<Rigidbody2D>().velocity = transform.rotation * Vector2.up * 5;
+            EndTurn();
         }
+
+		
 	}
 
     void EndTurn()
