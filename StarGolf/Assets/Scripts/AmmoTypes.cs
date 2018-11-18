28754a06a6e60e093abbbf2aaa4316de87ef0f0e
Pizzadrengen <39215307+Pizzadrengen@users.noreply.github.com>
2018-11-18 09:48:59 +0100

    1
    1

---
+++ StarGolf/Assets/Scripts/AmmoTypes.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AmmoTypes : MonoBehaviour {
+
+	public List<string> Ammotypes;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetAxis("Mouse ScrollWheel") > 0f)
+		{
+			foreach (string BallType in Ammotypes) ;
+			{
+				Debug.Log("New type");
+			}
+		}
+	}
+}
--- StarGolf/Assets/Scripts/GolfCannon.cs
+++ StarGolf/Assets/Scripts/GolfCannon.cs
@@ -2,71 +2,73 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class GolfCannon : MonoBehaviour {
+public class GolfCannon : MonoBehaviour
+{
 	public Camera cam;
 	public GameObject golfBallPrefab;
 
-    static List<GolfCannon> allPlayers = new List<GolfCannon>();
-    static int currentPlayerID;
-    int myID;
+	static List<GolfCannon> allPlayers = new List<GolfCannon>();
+	static int currentPlayerID;
+	int myID;
 	bool canshoot = true;
 	int CountClones = 0;
-    public static float shootCooldown = 0;
-    public static int turnCount = 0;
+	public static float shootCooldown = 0;
+	public static int turnCount = 0;
 
 	public BallType[] ammonitionType;
 
 	// Use this for initialization
-	void Start () {
-        myID = allPlayers.Count;
-        allPlayers.Add(this);
+	void Start()
+	{
+		myID = allPlayers.Count;
+		allPlayers.Add(this);
 	}
 
 	// Update is called once per frame
 	void Update()
 	{
-		
+
 		if (currentPlayerID == myID)
 		{
-            if (shootCooldown <= 0)
-            {
-		    	Vector2 mousePos = cam.ScreenToWorldPoint(Input.mousePosition) - transform.position;
-    			transform.rotation = Quaternion.Euler(0, 0, -Mathf.Atan2(mousePos.x, mousePos.y) * Mathf.Rad2Deg);
-    
-			    if (Input.GetKeyDown(KeyCode.Mouse0))
-			    {
-    				if (canshoot == true)
-				    {
-    					GameObject Ball = Instantiate(golfBallPrefab, transform.position, transform.rotation);
-					    Ball.GetComponent<Rigidbody2D>().velocity = transform.rotation * Vector2.up * 5;
-                        Ball.GetComponent<BallStuff>().Initialize(ammonitionType[0], 0, turnCount + 1);
-    
-                        shootCooldown = 10;
-					    EndTurn();
-				    }
-				    else
-				    {
-    					Debug.Log("You can not shoot right now!");
-				    }
-			    }
-            }
-            else
-            {
-                shootCooldown -= Time.deltaTime;
-            }
+			if (shootCooldown <= 0)
+			{
+				Vector2 mousePos = cam.ScreenToWorldPoint(Input.mousePosition) - transform.position;
+				transform.rotation = Quaternion.Euler(0, 0, -Mathf.Atan2(mousePos.x, mousePos.y) * Mathf.Rad2Deg);
+
+				if (Input.GetKeyDown(KeyCode.Mouse0))
+				{
+					if (canshoot == true)
+					{
+						GameObject Ball = Instantiate(golfBallPrefab, transform.position, transform.rotation);
+						Ball.GetComponent<Rigidbody2D>().velocity = transform.rotation * Vector2.up * 5;
+						Ball.GetComponent<BallStuff>().Initialize(ammonitionType[0], 0, turnCount + 1);
+
+						shootCooldown = 10;
+						EndTurn();
+					}
+					else
+					{
+						Debug.Log("You can not shoot right now!");
+					}
+				}
+			}
+			else
+			{
+				shootCooldown -= Time.deltaTime;
+			}
 		}
 
 	}
-    void EndTurn()
-    {
-        turnCount++;
-        currentPlayerID++;
+	void EndTurn()
+	{
+		turnCount++;
+		currentPlayerID++;
 
-        if (currentPlayerID >= allPlayers.Count)
-        {
-            currentPlayerID = 0;
-        }
+		if (currentPlayerID >= allPlayers.Count)
+		{
+			currentPlayerID = 0;
+		}
 
-        print(currentPlayerID);
-    }
+		print(currentPlayerID);
+	}
 }
